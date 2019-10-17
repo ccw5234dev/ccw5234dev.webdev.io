60c1b04213e766f9389cb2d25091fc39751ff37a
ccw5234dev <gameDevCcw24@gmail.com>
2019-10-17 11:19:34 -0400

    remove start js button

--- pursuit.js
+++ pursuit.js
@@ -1,11 +1,1 @@
-function createStartButton (){
-  let startButton = document.createElement("button");
-  startButton.innerHTML = "Start";
 
-  var body = document.getElementsByTagName("body")[0];
-  body.appendChild(startButton);
-
-  button.addEventListener("click", function goToGame(){
-    window.location.href = "pursuit.html";
-  })
-}
