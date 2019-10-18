8c865e01e264c34b2eefcd2c1f50c9beac8fefd6
ccw5234dev <gameDevCcw24@gmail.com>
2019-10-18 14:48:49 -0400

    create happiness bar

--- pursuit.js
+++ pursuit.js
@@ -1,1 +1,16 @@
 
+
+createHappinessBar();
+
+
+function createHappinessBar(){
+  var canvas = document.getElementById('canvas');
+  if(canvas.getContext){
+    var context = canvas.getContext('2d');
+
+    context.fillRect(20,20,100,100);
+    context.clearRect(40,40,60,60);
+    context.strokeRect(45,45,50,50);
+    context.rotate(20* Math.PI / 90);
+  }
+}
