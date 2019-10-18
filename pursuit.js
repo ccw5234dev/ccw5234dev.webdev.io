c15c1a3f681ff96ce7954e2e83497024dd5f5ed0
ccw5234dev <gameDevCcw24@gmail.com>
2019-10-18 15:00:00 -0400

    debug happiness bar#3

--- pursuit.js
+++ pursuit.js
@@ -1,12 +1,17 @@
 
 
-createHappinessBar();
+createDefaultHappinessBar();
 
 
-function createHappinessBar(){
+function createDefaultHappinessBar(){
   var canvas = document.getElementById('canvas');
   if(canvas.getContext){
     var context = canvas.getContext('2d');
+    var gradient = context.createLinearGradient(0,0,100,0);
+    gradient.addColorStop(0,"black");
+    gradient.addColorStop(1, "yellow");
+
+    context.fillStyle = gradient;
 
     context.fillRect(20,20,100,100);
     context.clearRect(40,40,60,60);
