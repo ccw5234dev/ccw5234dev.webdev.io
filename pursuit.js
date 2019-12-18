8debbfa0a6fbce31db0595abc8a79ab5f76ae05a
ccw5234dev <gameDevCcw24@gmail.com>
2019-12-17 19:50:46 -0500

    Updated site

--- pursuit.js
+++ pursuit.js
@@ -1,21 +1,28 @@
+let descriptionMenuText = {"You are new recruit in a kingdom’s military, you have been tasked by the military and your superiors to get to Suss Fal base, the nicest military base in the country that all new recruits wish to be deployed to, and deliver a secret package. You absolutely need to deliver this package; the lives of your comrades may depend on it.",
+"However, your superiors failed to tell you how to get there other than the relative direction and a few natural landmarks to help guide your way.",
+"The first natural landmark is the Decin Ding River, a mystical river where a dense fog lingers above it which envelops anything that it embraces.",
+"The second natural landmark is the Spirit Forest, a forest that contains a wide variety of spirits that usually are docile during the day, but more aggressive at night."
+};
 
 
-createDefaultHappinessBar();
+var questions = [
+{questionId:"question1",formName:"form1",radioName:"hangoutWFriendChoice1", values:["Peter (+50 happiness)","Thomas (+20 happiness)","Seth(+10 happiness)"]},
+{questionId:"question2",formName:"form2",radioName:"goToDoctorSickChoice2", values:["Get the shot(-10 happiness)","Freak out(-10 happiness)"]},
+{questionId:"question3",formName:"form3",radioName:"AskGirlOutChoice3", values:["Ask her out (+20 happiness)","Don't ask her out(-10 happiness)"]},
+{questionId:"question4",formName:"form4",radioName:"DiscussHalloweenChoice4", values:["Yes (+50 happiness)","No (-5 happiness)"]},
+{questionId:"question5",formName:"form5",radioName:"goToDoctorSickChoice5", values:["Get the shot(-10 happiness)","Freak out(-10 happiness)"],
+{questionId:"question6",formName:"form6",radioName:"AskDadToPlayChoice6", values:["Ask him (+20 happiness)","Don't ask him (-5 happiness)"]},
+{questionId:"question7",formName:"form7",radioName:"SnowDayFriendPlayChoice7", values:["Yes (+10 happiness)","No (-20 happiness)"]},
+{questionId:"question8",formName:"form8",radioName:"ChristmasPresentChoice8", values:["GameCube(+50 happiness)","GameBoy (+20 happiness)","Socks (-10 happiness)"]}
+];
 
 
-function createDefaultHappinessBar(){
-  var canvas = document.getElementById('canvas');
-  if(canvas.getContext){
-    var context = canvas.getContext('2d');
-    var gradient = context.createLinearGradient(0,0,100,0);
-    gradient.addColorStop(0,"black");
-    gradient.addColorStop(1, "yellow");
 
-    context.fillStyle = gradient;
-
-    context.fillRect(20,20,100,100);
-    context.clearRect(40,40,60,60);
-    context.strokeRect(10,10,5,5);
-    context.rotate(20* Math.PI / 180);
-  }
+function createFirstQuestion(){
+  var theInput = document.createElement("input");
+  theInput.setAttribute('type',"radio");
+  theInput.setAttribute('name',"firstQuest");
+  theInput.setAttribute('value',"John");
 }
+
+function
